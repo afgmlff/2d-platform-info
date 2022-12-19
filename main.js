9ae89e35b255568f8f7704fcec80f1198947d290
afgmlff <aeciofgm@gmail.com>
2022-12-19 19:14:14 -0300

    canvas init

---
+++ main.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = 700;
+
+
+
+    class Story{
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+        }
+
+        update(){
+
+        }
+
+        draw(){
+            
+        }
+    }
+});
+
+
+
+
+
