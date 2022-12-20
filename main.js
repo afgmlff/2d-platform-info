04327ff6aeb5f7554ff9bd1eab7cfa2dae106e67
afgmlff <aeciofgm@gmail.com>
2022-12-19 22:15:23 -0300

    key event handlers left n right

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { Player } from './player.js';
+import { InputHandler } from './input.js';
+
 window.addEventListener('load', function(){
     const canvas = document.getElementById("canvas1");
     const ctx = canvas.getContext("2d");
@@ -10,16 +13,31 @@ window.addEventListener('load', function(){
         constructor(width, height){
             this.width = width;
             this.height = height;
+            this.player = new Player(this);
+            this.input = new InputHandler();
         }
 
         update(){
-
+            this.player.update();
         }
 
-        draw(){
-            
+        draw(context){
+            this.player.draw(context);
         }
     }
+
+    const story = new Story(canvas.width, canvas.height);
+    console.log(story);
+
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        story.update()
+        story.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+
 });
 
 
--- player.js
+++ player.js
@@ -1,5 +1,19 @@
-class Player{
+export class Player{
     constructor(game){
         this.game = game;
+
+        this.width = 128;
+        this.height = 128;
+        this.x = 0;
+        this.y = this.game.height - this.height;
+        this.image = document.getElementById('player')
+    }
+
+    update(){
+        
+    }
+
+    draw(context){ //768 
+        context.drawImage(this.image, 0, 768, this.width, this.height, this.x, this.y, this.width, this.height) // alterar depois pra sheet do character 8bit
     }
 }
